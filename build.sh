f1297bc8ba252037194ed9848fea915fbcc2406a
Ubuntu <ubuntu@ip-172-31-47-218.us-east-2.compute.internal>
2024-05-15 13:31:04 +0000

    third commit

--- build.sh
+++ build.sh
@@ -15,16 +15,16 @@ docker build -t "$IMAGE_NAME" .
 docker login -u veera030 -p sindhuamu@98
 
 if [ "$BRANCH_NAME" == "devs" ]; then
-  docker tag "$IMAGE_NAME" "$veera030/dev:capstone"
-  docker push "$veera030/dev:capstone"
+  docker tag "$IMAGE_NAME" "$veera030/dev:tagname"
+  docker push "$veera030/dev:tagname"
   if [ $? -ne 0 ]; then
     echo "Failed to push Docker image to dev repository."
     exit 1
   fi
   echo "Docker image pushed to dev repository: $dev:latest"
 elif [ "$BRANCH_NAME" == "master" ]; then
-  docker tag "$IMAGE_NAME" "$veera030/prod:capstone"
-  docker push "$veera030/prod:capstone"
+  docker tag "$IMAGE_NAME" "$veera030/prod:tagname"
+  docker push "$veera030/prod:tagname"
   if [ $? -ne 0 ]; then
     echo "Failed to push Docker image to prod repository."
     exit 1
